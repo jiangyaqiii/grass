96762ef525fe6664657b22fd3d85cc9196869fa7
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-11-29 00:56:12 +0800

    Update grass_start.sh

--- grass_start.sh
+++ grass_start.sh
@@ -5,7 +5,19 @@ sudo apt install -y git
 ##安装python3.8
 sudo apt-get install -y software-properties-common
 echo -e "\n" |sudo add-apt-repository ppa:deadsnakes/ppa
-echo -e "2\n2" |apt install -y python3.8
+###########
+echo -e "2\n2" | sudo apt-get install -y expect
+# # 编写跳过选项脚本
+echo '#!/usr/bin/expect
+spawn sudo apt-get install -y python3.8
+expect "Geographic area:"
+send "2\r"
+expect "Time zone:"
+send "2\r"
+interact'>run_npm.expect
+chmod +x run_npm.expect
+./run_npm.expect
+###########
 ################################
 ##安装pip
 sudo apt install -y python3-pip
