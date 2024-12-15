ab3918c56e6356e0f8d4221e477df076a1124667
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-12-15 23:41:43 +0800

    Update grass_restart.sh

--- grass_restart.sh
+++ grass_restart.sh
@@ -1,5 +1,5 @@
 docker exec -d grass-contain /bin/bash -c "pkill -9 python"
 docker start grass-contain
-docker exec -d grass-contain /bin/bash -c "python3 grass/main.py"
+docker exec -d grass-contain /bin/bash -c "cd grass && python3 main.py"
 
 rm -f grass_restart.sh
